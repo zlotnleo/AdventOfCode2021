69fbde3502f376595d5bf99e1f05ad578e28810f
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2021-12-01 12:01:41 +0000

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        private static int Solve(IReadOnlyList<int> depths, int offset) =>
+            Enumerable.Range(0, depths.Count - offset)
+                .Count(i => depths[i + offset] > depths[i]);
+
+        public static void Main()
+        {
+            var depths = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+            Console.WriteLine(Solve(depths, 1));
+            Console.WriteLine(Solve(depths, 3));
+        }
+    }
+}
